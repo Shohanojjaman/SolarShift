1df5c1e4363668c89748e7d043283f410e701cc5
Shohanojjaman <shohanojjamanemon@gmail.com>
2026-06-05 01:37:19 +0600

    Add Cookies Consent Banner with accept and decline options

--- jQuery.js
+++ jQuery.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
+  // FAQ
   $('.question').click(function () {
     $(this).next('.answer').slideToggle(500);
     $(this).find('i').toggleClass('rotate');
+  });
+  // Cookies
+  $('#decline').click(() => {
+    $('.cookies').fadeOut(300);
+  });
+  $('#accept').click(() => {
+    $('.cookies').fadeOut(300);
   });
 });
